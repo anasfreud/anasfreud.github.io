6877f6d479b9800637f5f279afc590b97620c5eb
Anastasia Fedorova <79627149+anasfreud@users.noreply.github.com>
2022-10-09 13:28:55 +0300

    Update script.js

--- task5/script.js
+++ task5/script.js
@@ -1,32 +1,20 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Todo</title>
+'use strict';
 
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
-    <script defer src="script.js"></script>
+window.addEventListener('DOMContentLoaded', () => {
+  const inputs = document.querySelectorAll('input'),
+        btn = document.querySelector('button'),
+        total = document.querySelector('.total'),
+        priceInput = inputs[0],
+        countInput = inputs[1];
+  
+  let reg = /\D/;
+  btn.addEventListener('click', () => {
+    if (reg.test(priceInput.value) || reg.test(countInput.value)) {
+      total.textContent = 'Incorrect format';
+    } else {
+      total.textContent = +priceInput.value * +countInput.value;
+    }
+  });
 
-</head>
-<body>
 
-    <div class="d-flex flex-row">
-        <div class="d-flex flex-column justify-content-start align-items-start">
-            <input class="form-control m-3" id="count" type="text" style="width: 250px;">
-            <input class="form-control m-3" id="price" type="text" style="width: 250px;">
-            <button class="btn btn-primary m-3">Calculate</button>
-        </div>
-
-        <div class="m-3">
-            Your total is:
-            <div class="total">
-                -
-            </div>
-        </div>
-
-    </div>
-    
-</body>
-</html>
+});

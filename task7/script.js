9e611e85ce589b8159c33255aa011c9f1247d175
Anastasia Fedorova <79627149+anasfreud@users.noreply.github.com>
2022-10-25 12:06:15 +0300

    slick

--- task7/script.js
+++ task7/script.js
@@ -1,5 +1,19 @@
 $(document).ready(function() {
     $('.gallery').slick({
         dots: true,
+        infinite: true,
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        responsive: [
+            {
+              breakpoint: 768,
+              settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                fade: true,
+                cssEase: 'linear'
+              }
+            }
+        ],
     });
 });

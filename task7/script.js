ef5533a0fae85257ae5bf4ad775f0fdeb22b6433
Anastasia <79627149+anasfreud@users.noreply.github.com>
2022-10-27 13:26:00 +0300

    images added

--- task7/script.js
+++ task7/script.js
@@ -1,19 +1,26 @@
 $(document).ready(function() {
-    $('.gallery').slick({
-        dots: true,
-        infinite: true,
-        slidesToShow: 4,
-        slidesToScroll: 1,
-        responsive: [
-            {
-              breakpoint: 768,
-              settings: {
-                slidesToShow: 1,
-                slidesToScroll: 1,
-                fade: true,
-                cssEase: 'linear'
-              }
+
+  const gallery = $('.gallery');
+
+  for (let i = 1; i <= 16; i++) {
+    gallery.append(`<img src="images/img${i}.png" alt="img${i}">`);
+  }
+
+  gallery.slick({
+      dots: true,
+      infinite: false,
+      slidesToShow: 4,
+      slidesToScroll: 1,
+      responsive: [
+          {
+            breakpoint: 768,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 1
             }
-        ],
-    });
+          }
+      ],
+  });
+
+
 });
